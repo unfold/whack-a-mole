186967a1369f2118980da985c4a67292a64098d0
Einar Löve <einar.love@gmail.com>
2016-04-05 18:28:46 +0200

    add animations to register

--- src/components/AvatarPicker.js
+++ src/components/AvatarPicker.js
@@ -1,12 +1,14 @@
 import React, { Component, PropTypes } from 'react'
-import { StyleSheet } from 'react-look'
+import look, { StyleSheet } from 'react-look'
 import AvatarListItem from './AvatarListItem'
 import Button from './Button'
 import emojiMap from '../utils/emoji-map.json'
 import map from 'lodash/map'
+import cssEase from 'css-ease'
 
 let styles
 
+@look
 export default class AvatarPicker extends Component {
   static propTypes = {
     onSelectAvatar: PropTypes.func.isRequired,
@@ -32,17 +34,14 @@ export default class AvatarPicker extends Component {
 
   render() {
     if (!this.props.visible) {
-      return <div />
+      return <div className={styles.container} />
     }
 
     return (
       <div className={styles.container}>
-        { this.state.selected &&
-
-          <Button className={styles.button} onClick={this.onPick}>
-            Pick {emojiMap[this.state.selected]} as your avatar
-          </Button>
-        }
+        <Button className={styles.button} onClick={this.onPick}>
+          Pick {emojiMap[this.state.selected]} as your avatar
+        </Button>
 
         <div className={styles.avatarList}>
           {map(emojiMap, (emoji, key) => (
@@ -68,11 +67,18 @@ styles = StyleSheet.create({
     left: 0,
     width: '100%',
     height: '100%',
-    background: 'white',
 
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    transition: `opacity .4s ${cssEase['ease-out-cubic']}, transform 1s ${cssEase['ease-out-expo']}`,
+    transitionDelay: '.4s',
+
+    'visible=false': {
+      zIndex: -1,
+      opacity: 0,
+      transform: 'translate3d(0, 50%, 0)',
+    },
   },
 
   avatarList: {
@@ -88,5 +94,11 @@ styles = StyleSheet.create({
     bottom: '2em',
     left: '50%',
     transform: 'translateX(-50%)',
+    transition: `.7s transform ${cssEase['ease-out-expo']}, .2s opacity ${cssEase['ease-out-cubic']}`,
+
+    'selected=null': {
+      opacity: 0,
+      transform: 'translate3d(-50%, 2em, 0)',
+    },
   },
 })
