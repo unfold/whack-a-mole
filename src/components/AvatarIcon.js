9820f1373b31f2740d3f13cd24dc15b907c19cf7
Simen Brekken <simen@unfold.no>
2016-04-05 20:03:26 +0200

    Add SVG avatars

---
+++ src/components/AvatarIcon.js
@@ -0,0 +1,26 @@
+import React, { Component, PropTypes } from 'react'
+import shuffle from 'lodash/shuffle'
+
+const avatars = require.context('../assets/avatars')
+
+export default class AvatarIcon extends Component {
+  static propTypes = {
+    id: PropTypes.string,
+  }
+
+  static keys = shuffle(avatars.keys())
+
+  render() {
+    const { id, ...props } = this.props
+
+    if (!id) {
+      return null
+    }
+
+    const Avatar = avatars(id)
+
+    return (
+      <Avatar {...props} />
+    )
+  }
+}
